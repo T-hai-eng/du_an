39e740cb8951515b6ca4892a3c198b26e8f940b0
nguyetha1609 <nguyetha.1609@gmail.com>
2024-10-20 14:49:01 +0700

    ...

---
+++ qlhtdn/resources/views/login.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+</head>
+<body>
+    <div class="container">
+        <div class="toolbar">
+            <a href="index">Công ty M-offer</a>
+            <a href="contact">Liên Lạc</a>
+            <a href="blog">Blog</a>
+            <a href="product">Sản phẩm</a>
+        </div>
+        <center class="total">
+            <div class="lg">
+                <div class="logo" style="background-color: #fff;">
+                    <img src="images/Screenshot 2024-09-30 195100.png" alt="" width="80px" height="80px">
+                </div>
+                <h3>Đăng nhập</h3>
+                <div class="form-login">
+                    <form action="method" class="login">
+                        <input type="text" name="username" placeholder="Tên đang nhập:"><br>
+                        <input type="text" name="password" placeholder="Mật khẩu:"><br>
+                        <a href="forgot-password">Quên mật khẩu?</a>
+                        <center>
+                            <button class="click">Đăng nhập</button>
+                        </center>
+                    </form>
+                </div>
+            </div>
+        </center>
+    </div>
+</body>
+</html>
